18e7fa41482830451c3804e6bdc107831591040c
gurpreet1426 <gurpreetaws@gmail.com>
2025-05-15 00:07:02 +0530

    code changes

--- server.js
+++ server.js
@@ -1,32 +1,11 @@
-const express = require("express");
-//const cors = require("cors");
-const app = express();
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-//app.use(cors(corsOptions));
-// parse requests of content-type - application/json
-app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-
-//const databaseSync= require("./config/databasesync");
-//databaseSync.authusersdb();
-//databaseSync.productDL();
-
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Plant Agro tech application." });
-});
-// const router = require('./app/routes/router');
-// app.use('/', router);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+    const express= require('express');
+    const app= express();
+    app.get('/api/get',(req,res)=>{
+        res.send({message:'nodejs aws deployement'})
+    })
+    app.get('/api/get/users',(req,res)=>{
+        res.send({name:'Dev',email:'test@gmail.com','role':'developer'})
+    })
+    app.listen(9000,(req,res)=>{
+        console.log('server running on pot 9000')
+    })
