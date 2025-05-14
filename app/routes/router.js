605a5061acbc8d9e83f9363fd72f3663fc1785b9
gurpreet1426 <gurpreetaws@gmail.com>
2025-05-15 00:38:38 +0530

    code change

---
+++ app/routes/router.js
@@ -0,0 +1,7 @@
+//const authUserController = require("../controllers/userController/authuser.controller");
+const productController= require("../controllers/productController/product.controller");
+const express = require('express');
+const router = express.Router();
+//router.use('/api/authuser',authUserController);
+router.use('/api/product',productController);
+module.exports = router;
--- server.js
+++ server.js
@@ -14,9 +14,12 @@
     //     res.send({name:'Dev',email:'test@gmail.com','role':'developer'})
     // })
     // simple route
-    app.get("/", (req, res) => {
+    app.get("/api", (req, res) => {
     res.json({ message: "Welcome to Plant Agro tech application." });
     });
-    app.listen(8080,(req,res)=>{
-        console.log('server running on pot 9080')
+    const router = require('./app/routes/router');
+    app.use('/', router);
+    const PORT = 8080;
+    app.listen(PORT,(req,res)=>{
+        console.log('server running on pot 8080')
     })
