fbdc974c89ee69fad82264cb12498281a56be8c1
gurpreet1426 <gurpreetaws@gmail.com>
2025-05-15 00:24:17 +0530

    code change

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
     const express= require('express');
+    const cors = require("cors");
     const app= express();
-    app.get('/api/get',(req,res)=>{
-        res.send({message:'nodejs aws deployement'})
-    })
-    app.get('/api/get/users',(req,res)=>{
-        res.send({name:'Dev',email:'test@gmail.com','role':'developer'})
-    })
+    var corsOptions = {
+        origin: "http://localhost:8081"
+    };
+    app.use(cors(corsOptions));
+    app.use(express.json());
+
+    // app.get('/api/get',(req,res)=>{
+    //     res.send({message:'nodejs aws deployement'})
+    // })
+    // app.get('/api/get/users',(req,res)=>{
+    //     res.send({name:'Dev',email:'test@gmail.com','role':'developer'})
+    // })
+    // simple route
+    app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Plant Agro tech application." });
+    });
     app.listen(8080,(req,res)=>{
         console.log('server running on pot 9080')
     })
