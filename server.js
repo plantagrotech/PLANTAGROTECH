99749813048d98e3d8e0141d994a346a3bf2507c
gurpreet1426 <gurpreetaws@gmail.com>
2025-05-15 00:17:15 +0530

    code changes

--- server.js
+++ server.js
@@ -6,6 +6,6 @@
     app.get('/api/get/users',(req,res)=>{
         res.send({name:'Dev',email:'test@gmail.com','role':'developer'})
     })
-    app.listen(9000,(req,res)=>{
-        console.log('server running on pot 9000')
+    app.listen(8080,(req,res)=>{
+        console.log('server running on pot 9080')
     })
